34b596705e85422652669178a3c1952644c4ec4d
Sumaiiya-Haque <sumaiyahaque475@gmail.com>
2025-10-07 00:19:40 +0600

    search btn completely done

--- src/Pages/Products.jsx
+++ src/Pages/Products.jsx
@@ -3,15 +3,18 @@ import useProducts from "../Hooks/useProducts";
 import ProductCard from "../Components/ProductCard";
 
 const Products = () => {
-  const [search,setSearch] = useState('');
+  
   const { products } = useProducts();
+  const [search,setSearch] = useState('');
+  const term = search.trim().toLocaleLowerCase()
+  const searchedProducts = term ? products.filter(product=> product.name.toLocaleLowerCase().includes(term)):products 
   return (
     <div>
       <div className="flex justify-between items-center py-5 ">
         <h1 className="text-3xl font-semibold">
           All Products{" "}
           <span className="text-sm text-gray-500">
-            ({products.length})Products Found
+            ({searchedProducts.length})Products Found
           </span>
         </h1>
         <label className="input">
@@ -22,7 +25,7 @@ const Products = () => {
         </label>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        {products.map((product) => (
+        {searchedProducts.map((product) => (
           <ProductCard key={product.id} product={product}></ProductCard>
         ))}
       </div>
