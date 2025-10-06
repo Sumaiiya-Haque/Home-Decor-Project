462cfe82a1b7510ba0634daffd251fd279ab7326
Sumaiiya-Haque <sumaiyahaque475@gmail.com>
2025-10-06 13:17:00 +0600

    navbar design

---
+++ src/Pages/ErrorPage.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { useRouteError } from 'react-router';
+import Navbar from '../Components/Navbar';
+import Footer from '../Components/Footer';
+
+const ErrorPage = () => {
+    const error = useRouteError()
+    return (
+        <div>
+             <Navbar></Navbar>
+             <h1>{error.message}</h1>
+            <Footer></Footer>
+           
+        </div>
+    );
+};
+
+export default ErrorPage;
---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,33 @@
+import { createBrowserRouter } from "react-router";
+
+import Home from "../Pages/Home";
+import Products from "../Pages/Products";
+import MainLayout from "../Layouts/MainLayout";
+import ErrorPage from "../Pages/ErrorPage";
+
+// named export
+
+ const router = createBrowserRouter([
+      {
+    path: '/',
+    element: <MainLayout></MainLayout>,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+  {
+    path: '/',
+    element: <Home></Home>,
+  },
+    {
+    path: '/products',
+    element: <Products></Products>,
+  },
+    ]
+  },
+//   {
+//     path: '*',
+//     element: <ErrorPage></ErrorPage>
+//   }
+])
+
+export default router;
+
